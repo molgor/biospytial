3e0a443a6c373056e9db6733641e7d598e8d75dd
Juan Escamilla <juan@localhost.localdomain>
2017-03-06 19:22:06 +0000

    Fixed Issue with dynamic selection of Grids and scales using the initMesh(int) method. Now it works very well using an abstracted table.

--- notes_examples/explore_gridded_taxonomy.py
+++ notes_examples/explore_gridded_taxonomy.py
@@ -8,9 +8,9 @@ import logging
 
 logger = logging.getLogger('biospytial.mesh.tools')
 
-m1 = initMesh(7)
-m1
-m1.objects.all()
+#m1 = initMesh(7)
+#m1
+#m1.objects.all()
 biosphere = Occurrence.objects.all()
 
 
@@ -18,20 +18,11 @@ from sketches.models import Country
 mexico_border = Country.objects.filter(name__contains='exico').get()
 mexbiosphere = biosphere.filter(geom__intersects=mexico_border.geom)
 
-m1mexico = m1.objects.filter(cell__intersects=mexico_border.geom)
+#m1mexico = m1.objects.filter(cell__intersects=mexico_border.geom)
 
-mexgrid = MexMesh.objects.filter(cell__intersects=mexico_border.geom)
+#mexgrid = MexMesh.objects.filter(cell__intersects=mexico_border.geom)
 #gg = GriddedTaxonomy(biosphere=mexbiosphere,mesh=m1.objects.all())
 
 #gg = GriddedTaxonomy(biosphere=mexbiosphere,mesh=m1mexico)
 
-#migrateGridToNeo(MexMesh,intersect_with=mexico_border.geom)
-
-for i in range(1,7):
-    m =  initMesh(i)
-    c = m.objects.all()[0]
-    logger.info("INSERTING MESH %s IN THE GRAPH DATABASE"%c.getScaleLevel())
-    del(m)
-    
-    
     
